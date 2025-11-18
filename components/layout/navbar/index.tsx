c21e1ce38b1f343e99c92fde110b033ad10a21b5
Jaffer720 <mjaffer720@gmail.com>
2025-11-18 15:27:35 +0500

    fix: resolve navigation path issues

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -11,7 +11,6 @@ const { SITE_NAME } = process.env;
 
 export async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
-
   return (
     <nav className="relative flex items-center justify-between p-4 lg:px-6">
       <div className="block flex-none md:hidden">
@@ -36,7 +35,7 @@ export async function Navbar() {
               {menu.map((item: Menu) => (
                 <li key={item.title}>
                   <Link
-                    href={item.path}
+                    href={`/${item.path}`}
                     prefetch={true}
                     className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
                   >
